374a4f6d9bb603d1cde8ae36bb6f57b373327674
Oleh Kapral <Oleh.Kapral@symphony-solutions.eu>
2015-12-14 09:35:11 +0200

    merge

--- src/app/home/home.controller.js
+++ src/app/home/home.controller.js
@@ -10,5 +10,10 @@
     var vm = this;
     vm.personImages = ['http://placehold.it/200x100','http://placehold.it/200x100','http://placehold.it/200x100','http://placehold.it/200x100'];
     vm.images = ['../../assets/images/test.png','http://placehold.it/100x100','http://placehold.it/100x100','http://placehold.it/100x100']
+    angular.element('#image-cropper').cropit({
+      imageState: {
+        src: 'http://lorempixel.com/500/400/',
+      }
+    });
   }
 })();
